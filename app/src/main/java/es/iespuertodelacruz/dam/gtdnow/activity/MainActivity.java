3afd1d1c87f551b03f8803526474902639dea34e
ehedei <ehedeihg@gmail.com>
2019-05-30 14:04:12 +0100

    Changes in colours

--- app/src/main/java/es/iespuertodelacruz/dam/gtdnow/activity/MainActivity.java
+++ app/src/main/java/es/iespuertodelacruz/dam/gtdnow/activity/MainActivity.java
@@ -12,6 +12,7 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 import java.util.Arrays;
 import java.util.List;
 
